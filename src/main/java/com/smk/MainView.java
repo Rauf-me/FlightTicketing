062da0ceda4e806da9a9e8bffa0ce9ca9846abfe
Rauf <r.wirapandita@gmail.com>
2023-11-15 14:33:27 +0700

    Inisialized

---
+++ src/main/java/com/smk/MainView.java
@@ -0,0 +1,43 @@
+package com.smk;
+
+import com.smk.view.CreateBooking;
+import com.smk.view.SearchBooking;
+import com.vaadin.flow.component.applayout.AppLayout;
+import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.html.H1;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouterLink;
+import com.vaadin.flow.theme.lumo.LumoUtility;
+
+@Route
+public class MainView extends AppLayout {
+
+    public MainView(){
+        createHeader();
+        createDrawer();
+    }
+    private void  createHeader(){
+        H1 logo = new H1("Booking Flight");
+        logo.addClassName(
+                LumoUtility.FontSize.LARGE
+        );
+        var header = new HorizontalLayout(
+                new DrawerToggle(), logo
+        );
+        header.setDefaultVerticalComponentAlignment(
+                FlexComponent.Alignment.CENTER
+        );
+        header.expand(logo);
+        header.setWidthFull();
+        addToNavbar(header);
+    }
+
+    private void createDrawer() {
+        addToDrawer(new VerticalLayout(new RouterLink("Create booking", CreateBooking.class),
+                new RouterLink("Search booking", SearchBooking.class)));
+    }
+
+}
---
+++ src/main/java/com/smk/view/CreateBooking.java
@@ -0,0 +1,30 @@
+package com.smk.view;
+
+import com.smk.MainView;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Create Booking")
+@Route(value = "create-booking", layout = MainView.class)
+public class CreateBooking extends VerticalLayout{
+    public CreateBooking() {
+        createForm();
+    }
+
+    private void createForm() {
+        setAlignItems(Alignment.STRETCH); // Code untuk mengubah ukuran/posisi dari form
+        ComboBox fromComboBox = new ComboBox("Dari");
+        ComboBox toComboBox = new ComboBox("Ke");
+        DatePicker departureDatePicker = new DatePicker("Tanggal Keberangkatan"); // Code untuk menambahkan tanggal
+        DatePicker arriveDatePicker = new DatePicker("Tanggal Kepulangan"); // "Label" untuk menambahkan text
+
+        Button SearchButton = new Button("Search");
+        SearchButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        add(fromComboBox, toComboBox, departureDatePicker, arriveDatePicker, SearchButton); // Note: Code harus teratur dengan code diatas karena menggunakan extends VerticalLayout (atas }
+    }
+}
